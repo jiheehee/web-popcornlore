e93e4c78535d289ece6b2820a645ca0021f67385
You JiHee <youjh0421@gmail.com>
2024-12-06 01:59:19 +0900

    초기 생성 241206

---
+++ src/main/java/com/web/popcornlore/PopcornloreApplication.java
@@ -0,0 +1,24 @@
+package com.web.popcornlore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornloreApplication {
+
+	public static void main(String[] args) {
+
+//		자료형 함수(){ 	~> 자료형은 return값의 자료형, return값이 없을땐 void
+//			로직~
+//			로직~
+//			return @@
+//		}
+
+		SpringApplication.run(PopcornloreApplication.class, args);
+
+		String animal="돌고래";
+		System.out.println(animal);
+
+	}
+
+}
---
+++ src/test/java/com/web/popcornlore/PopcornloreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.popcornlore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornloreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
