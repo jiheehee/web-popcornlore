9c1912e0547645d8ad8a98ec1da8c9f7315fec8d
You JiHee <youjh0421@gmail.com>
2025-02-28 18:40:11 +0900

    250228 jpa 레파지토리 연결

--- src/main/java/com/web/popcornlore/ItemController.java
+++ src/main/java/com/web/popcornlore/ItemController.java
@@ -1,5 +1,6 @@
 package com.web.popcornlore;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -7,16 +8,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @GetMapping("/list")
+//    String list(Model model){
+////        model.addAttribute("전달할데이터 이름","전달할 데이터");
+//        Scanner itemRepository;
+////        List<Item> result = itemRepository.findAll();
+//        model.addAttribute("name","홍길동");
+//        return "list.html";
+//    }
 
     @GetMapping("/list")
     String list(Model model){
-//        model.addAttribute("전달할데이터 이름","전달할 데이터");
-        model.addAttribute("name","홍길동");
+        var result=itemRepository.findAll();
+        System.out.println(result.get(0).title);
+        System.out.println(result.get(0).price);
+
+        model.addAttribute("name","오로라공주");
         return "list.html";
     }
 
@@ -27,9 +43,11 @@ public class ItemController {
 
     @PostMapping("/add") // url작명시 명사가 좋음!
     String addPost(@RequestParam String title,
-                   @RequestParam Integer price){
-        System.out.println(title);
-        System.out.println(price);
+                   @RequestParam Integer price){ //여기 적은 타입은, 적은대로 타입을 변환해달라는 것이기 때문에, 변환 원하는거 맘대로 집어넣으쇼
+//        System.out.println(title);
+//        System.out.println(price);
+//        new Item()
+//        itemRepository.save(???);  //Item테이블에 행으로 넣어줌 , 괄호 안에는 Item의 object를 넣어야 함
         return "redirect:/list";
     }
 
---
+++ src/main/java/com/web/popcornlore/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.web.popcornlore;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
+
+
+
+}
