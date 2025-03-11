aa4c651c16b06837a48b7baba31fc3078f2eb7ed
You JiHee <youjh0421@gmail.com>
2025-03-11 15:56:35 +0900

    250311 Notice 엔티티 수정

--- src/main/java/com/web/popcornlore/Notice.java
+++ src/main/java/com/web/popcornlore/Notice.java
@@ -5,14 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
-public class notice {
+public class Notice {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    public String title;
-    public Date date;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String noticeTitle;
+    private Date noticeDate;
 
 }
