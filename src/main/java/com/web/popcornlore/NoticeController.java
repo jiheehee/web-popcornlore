059f754d1b071b24d42d2f2ccd305f696e26fcad
You JiHee <youjh0421@gmail.com>
2025-03-14 19:13:40 +0900

    250313 공지사항

--- src/main/java/com/web/popcornlore/NoticeController.java
+++ src/main/java/com/web/popcornlore/NoticeController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,7 +32,27 @@ public class NoticeController {
         model.addAttribute("notice",result);
 
         return "/notice.html";
+    }
 
+    //noticeWrite.html에서 공지사항 글쓴거 고대로 noticeWrite.html로 보내기
+    @GetMapping("/noticeWrite")
+    String noticeWrite(String noticeTitle){
+        Notice notice=new Notice();
+        notice.setNoticeTitle(noticeTitle);
+        return "/noticeWrite";
+    }
+
+    @GetMapping("/noticeWritePage")
+    String noticeWrtiePage(){
+        return "/noticeWrite.html";
+    }
+
+    //noticeWrite.html에 있는거 받아서 notice.html로 보내줌 (공지사항 게시판으로 보내줌)
+    @PostMapping("/noticeAdd")
+    String noticeAdd(@RequestParam Long id,
+                     @RequestParam String noticeTitle,
+                     @RequestParam String noticeDate){
+        return "redirect:/notice";
     }
 
 
